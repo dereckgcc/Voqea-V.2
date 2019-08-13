0d78ae59fc7b781ca13ea479828351b6be64ec94
dereckgcc <49879156+dereckgcc@users.noreply.github.com>
2019-08-12 21:16:07 -0600

    Update userRoutes.js

--- BackEnd/src/routes/userRoutes.js
+++ BackEnd/src/routes/userRoutes.js
@@ -4,7 +4,7 @@ var express = require('express');
 var UserController = require('../controllers/userController');
 var mdAuth = require('../middlewares/authenticated');
 var multiparty = require('connect-multiparty');
-var mdSubir = multiparty({uploadDir: './src/uploads/users'});
+var md_subir = multiparty({uploadDir: './src/uploads/users'});
 
 //RUTAS
 var api = express.Router();
@@ -16,5 +16,7 @@ api.get('/listarUsuarios', mdAuth.ensureAuth, UserController.listarUsuarios);
 api.post('/crearUsuario', mdAuth.ensureAuth, UserController.crearUsuario);
 api.put('/editarUsuario/:id', mdAuth.ensureAuth, UserController.editarUsuario);
 api.delete('/eliminarUsuario/:id', mdAuth.ensureAuth, UserController.eliminarUsuario);
+api.post('/subir-imagen-usuario/:id', md_subir, UserController.subirImagenUser);
+api.get('/obtener-imagen-usuario/:nombreImagen', UserController.obtenerImagenUser);
 
 module.exports = api;
