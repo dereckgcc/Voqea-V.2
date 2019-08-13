22e0fe1441f4db640d9d9e8bae42af7e6cf0707f
dereckgcc <49879156+dereckgcc@users.noreply.github.com>
2019-08-12 21:16:03 -0600

    Update app.module.ts

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistroComponent } from './components/registro/registro.component';
+import { TaskComponent } from './components/task/task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistroComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule
