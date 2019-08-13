13123081abcc10d48f366e76a8cca180b88d4e11
dereckgcc <49879156+dereckgcc@users.noreply.github.com>
2019-08-13 08:52:39 -0600

    Update registro.component.ts

--- FrontEnd/src/app/components/registro/registro.component.ts
+++ FrontEnd/src/app/components/registro/registro.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/models/user.model'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.scss']
+  styleUrls: ['./registro.component.scss'],
+  providers: [UserService]
 })
 export class RegistroComponent implements OnInit {
+  public user:User;
+  public status;
 
-  constructor() { }
+
+  constructor(
+    private _router: Router,
+    private _userService: UserService
+  ) { 
+    this.user = new User ("","","","","","","","",0,"","",0,"")
+  }
 
   ngOnInit() {
+  }
+
+  registrar(){
+    this._userService.registro(this.user).subscribe(
+      response=>{
+        if(response){
+          this.status =  'ok'
+          console.log(response.user)
+          this._router.navigate(['/login'])
+        }
+      },
+      error=>{
+        console.log(<any>error);
+        this.status = "error"
+      }
+    )
   }
 
 }
