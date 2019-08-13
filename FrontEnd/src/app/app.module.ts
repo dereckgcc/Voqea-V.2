1fe84c751fa4e21ee8cb1f8f36fc18bfd2c60825
dereckgcc <49879156+dereckgcc@users.noreply.github.com>
2019-08-13 08:53:03 -0600

    Update app.module.ts

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -1,22 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';import{ FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { routing, appRoutingProviders } from "./app.routing";
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegistroComponent } from './components/registro/registro.component';
 import { TaskComponent } from './components/task/task.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegistroComponent,
-    TaskComponent
+    TaskComponent,
+    PerfilComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
